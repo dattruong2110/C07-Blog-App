5ed9b053f43f014fbc14a3e77b3a56caf536678e
Trương Tấn Đạt <tandattruong2110@gmail.com>
2024-06-03 00:43:54 +0700

    Implement HomePageBlog UI

--- src/components/home-page/HomePage.jsx
+++ src/components/home-page/HomePage.jsx
@@ -1,9 +1,11 @@
+import HomePageBlog from "./home-page-blog/HomePageBlog";
 import HomePageFact from "./home-page-fact/HomePageFact";
 
 const HomePage = () => {
   return (
     <>
       <HomePageFact />
+      <HomePageBlog />
     </>
   );
 };
