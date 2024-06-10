fac019107a8100201d707ccbb12f9774d716e24f
Anhtuan2190 <tuanva2190@gmail.com>
2024-06-10 20:15:31 +0700

    page-fact

--- src/router/PublicRoutes.jsx
+++ src/router/PublicRoutes.jsx
@@ -1,8 +1,12 @@
 import { Route, Routes } from "react-router";
 import HomePage from "../components/home-page/HomePage";
 import UserLayout from "../layouts/UserLayout";
+import FactPage from "../components/fact-page/FactPage";
 
-const ROUTE_DATA = [{ path: "/", element: HomePage, layout: UserLayout }];
+const ROUTE_DATA = [
+  { path: "/", element: HomePage, layout: UserLayout },
+  {path: "/fact", element: FactPage, layout: UserLayout}
+];
 
 const PublicRoutes = () => {
   return (
